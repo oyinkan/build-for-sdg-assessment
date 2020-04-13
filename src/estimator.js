1cce748956a005a966a888b22656fd13d5d3816e
oyinkan <skusman@student.lautech.edu.ng>
2020-04-13 02:05:37 +0100

    challenge one completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,10 @@
 const covid19ImpactEstimator = (data) => {
-
     let impact = {
         currentlyInfected: data.reportedCases * 10,
         get infectionsByRequestedTime() {
             return this.currentlyInfected * 512;
         }
     }
-
     let severeImpact = {
         currentlyInfected: data.reportedCases * 50,
 
